d9101fad8b2900d214fb59bf2aa814ff5dfd23f8
Josue <josuepereztorr@gmail.com>
2022-11-12 16:28:58 -0600

    Create and Delete Blogs/Posts

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -16,5 +16,68 @@ namespace BlogsAuth.Controllers
         public HomeController(Model.BloggingContext db) => _bloggingContext = db;
 
         public IActionResult Index() => View(_bloggingContext.Blogs.OrderBy(b => b.Name));
+        
+        public IActionResult BlogDetail(int id) => View(new Model.PostViewModel
+        {
+            blog = _bloggingContext.Blogs.FirstOrDefault(b => b.BlogId == id),
+            Posts = _bloggingContext.Posts.Where(p => p.BlogId == id)
+        });
+        
+        public IActionResult AddBlog() => View();
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult AddBlog(Model.Blog model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (_bloggingContext.Blogs.Any(b => b.Name == model.Name))
+                {
+                    ModelState.AddModelError("", "Name must be unique");
+                }
+                else
+                {
+                    _bloggingContext.AddBlog(model);
+                    return RedirectToAction("Index");
+                }
+            }
+
+            return View();
+        }
+        
+        public IActionResult DeleteBlog(int id)
+        {
+            _bloggingContext.DeleteBlog(_bloggingContext.Blogs.FirstOrDefault(b => b.BlogId == id));
+            return RedirectToAction("Index");
+        }
+        
+        public IActionResult AddPost(int id)
+        {
+            ViewBag.BlogId = id;
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult AddPost(int id, Model.Post post)
+        {
+            post.BlogId = id;
+            if (ModelState.IsValid)
+            {
+                _bloggingContext.AddPost(post);
+                return RedirectToAction("BlogDetail", new { id = id });
+            }
+            @ViewBag.BlogId = id;
+            return View();
+        }
+        
+        public IActionResult DeletePost(int id)
+        {
+            Model.Post post = _bloggingContext.Posts.FirstOrDefault(p => p.PostId == id);
+            int BlogId = post.BlogId;
+            _bloggingContext.DeletePost(post);
+            return RedirectToAction("BlogDetail", new { id = BlogId });
+        }
     }
 }
+
--- Models/Model.cs
+++ Models/Model.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using Microsoft.EntityFrameworkCore;
 
 namespace BlogsAuth.Models
@@ -11,11 +12,36 @@ namespace BlogsAuth.Models
 
             public DbSet<Blog> Blogs { get; set; }
             public DbSet<Post> Posts { get; set; }
+            
+            public void AddBlog(Blog blog)
+            {
+                this.Add(blog);
+                this.SaveChanges();
+            }
+            
+            public void DeleteBlog(Model.Blog blog)
+            {
+                this.Remove(blog);
+                this.SaveChanges();
+            }
+            
+            public void AddPost(Post post)
+            {
+                this.Add(post);
+                this.SaveChanges();
+            }
+            
+            public void DeletePost(Post post)
+            {
+                this.Remove(post);
+                this.SaveChanges();
+            }
         }
 
         public class Blog
         {
             public int BlogId { get; set; }
+            [Required]
             public string Name { get; set; }
 
             public ICollection<Post> Posts { get; set; }
@@ -24,11 +50,18 @@ namespace BlogsAuth.Models
         public class Post
         {
             public int PostId { get; set; }
+            [Required]
             public string Title { get; set; }
             public string Content { get; set; }
 
             public int BlogId { get; set; }
             public Blog Blog { get; set; }
+        }
+        
+        public class PostViewModel
+        {
+            public Blog blog { get; set; }
+            public IEnumerable<Post> Posts { get; set; }
         }
     }
 }
